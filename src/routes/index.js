ae410a4edbc6be610b073e5ce370eccee2be2489
reedlex98 <elder_louzada@outlook.com>
2019-06-15 17:45:05 -0300

    formularios de file adicionados

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 
 const router = express.Router();
+const app = express()
 
+app.use(express.static('../src/view'))
+
+// const indexHTML = require('../src/view/index.html')
 router.get('/', function (req, res, next) {
-    res.status(200).send({
-        'status': 'UP',
-        'title': 'Hackaton FCamara API'
-    });
+    
+    res.sendFile(__dirname+'../view/index.html')
+    
+    // res.status(200).send({
+    //     'status': 'UP',
+    //     'title': 'Hackaton FCamara API'
+    // });
 });
+
+router.get('/home', function (req,res,next) {
+})
+
+// router.get('/filas',function(req, res,next){
+//     res.send(indexHTML)
+// })
 
 module.exports = router;
